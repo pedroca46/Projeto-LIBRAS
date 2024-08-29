b6578d2848766175a2daae9cf7381debf7c88dd9
Pedro Alves <pedro.alves6491@gmail.com>
2024-08-29 00:11:59 -0300

    .

--- utils.py
+++ utils.py
@@ -28,9 +28,6 @@ def mediapipe_detection(image, holistic):
     return image, results
 
 def draw_styled_landmarks(image, results):
-    #mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
-                             #mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-                             #mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)) 
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
                              mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
                              mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)) 
@@ -43,7 +40,6 @@ def draw_styled_landmarks(image, results):
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
-    #face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, lh, rh])
